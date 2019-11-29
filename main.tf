a8172d452796287fd520d5f66efc6c2e3b44a4d6
14traits <john.rogers@mac.com>
2019-11-29 09:43:46 -0600

    fixed S3 bucket typo

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-14traits"
+    bucket = "terraform-demo-14traits"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
